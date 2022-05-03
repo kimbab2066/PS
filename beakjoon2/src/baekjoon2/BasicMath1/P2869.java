7daf03dbe43c61b6a6d1712efc6b8d7ffc385da6
kimbab2066 <qseft523125@g.yju.ac.kr>
2022-05-03 21:53:33 +0900

    2869번 달팽이

--- beakjoon2/src/baekjoon2/BasicMath1/P2869.java
+++ beakjoon2/src/baekjoon2/BasicMath1/P2869.java
@@ -1,14 +1,18 @@
 package baekjoon2.BasicMath1;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class P2869 {
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		int up = in.nextInt();
-		int down = in.nextInt();
-		int length = in.nextInt();
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int up = Integer.parseInt(st.nextToken());
+		int down = Integer.parseInt(st.nextToken());
+		int length = Integer.parseInt(st.nextToken());
 
 		int day = (length - down) / (up - down);
 		if ((length - down) % (up - down) != 0) day++;
