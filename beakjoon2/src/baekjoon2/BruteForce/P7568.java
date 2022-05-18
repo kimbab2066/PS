676dace7c3004ada910af04078b76cf9edf65729
Park <qseft523125@g.yju.ac.kr>
2022-05-18 09:48:47 +0900

    7568번 덩치

---
+++ beakjoon2/src/baekjoon2/BruteForce/P7568.java
@@ -0,0 +1,43 @@
+package baekjoon2.BruteForce;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class P7568 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		/*
+		 * 2 ≤ N ≤ 50 10 ≤ x, y ≤ 200
+		 */
+		int N = Integer.parseInt(br.readLine());
+
+		StringTokenizer st;
+		int[][] arr = new int[N][2];
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			arr[i][0] = Integer.parseInt(st.nextToken());
+			arr[i][1] = Integer.parseInt(st.nextToken());
+		}
+
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 0; i < N; i++) {
+			int rank = 1;
+
+			for (int j = 0; j < N; j++) {
+				if (i == j)
+					continue;
+
+				if (arr[i][0] < arr[j][0] && arr[i][1] < arr[j][1]) {
+					rank++;
+				}
+			}
+			sb.append(rank).append(" ");
+		}
+
+		System.out.println(sb);
+	}// end of main
+}
+// end of class
