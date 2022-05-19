1df9b47b8add293f73945dcc6ab08a595b44493b
kimbab2066 <qseft523125@g.yju.ac.kr>
2022-05-19 12:35:02 +0900

    1436번 영화감독 숌

---
+++ beakjoon2/src/baekjoon2/BruteForce/P1436.java
@@ -0,0 +1,11 @@
+package baekjoon2.BruteForce;
+
+public class P1436 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        System.out.println();
+
+
+    }//end of main
+}//end of class
