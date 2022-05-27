108a8697d62da8fc9f8ed0fcaabcb18210ae5974
kimbab2066 <qseft523125@g.yju.ac.kr>
2022-05-27 13:07:25 +0900

    11651번 좌표 정렬하기 2

---
+++ beakjoon2/src/baekjoon2/Sort/P11651.java
@@ -0,0 +1,36 @@
+package baekjoon2.Sort;
+
+import java.io.BufferedReader;
+import java.io.IOError;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class P11651 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st;
+		int arr[][] = new int[N][2];
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			arr[i][0] = Integer.parseInt(st.nextToken());
+			arr[i][1] = Integer.parseInt(st.nextToken());
+		} // end of for
+
+		Arrays.sort(arr, (e1, e2) -> {
+			if (e1[1] == e2[1]) {
+				return e1[0] - e2[0];
+			} else {
+				return e1[1] - e2[1];
+			}
+		});
+		StringBuilder sb = new StringBuilder();
+
+		for (int val[] : arr) {
+			sb.append(val[0] + " " + val[1]).append("\n");
+		}
+		System.out.println(sb);
+	}// end of main
+}// end of class
