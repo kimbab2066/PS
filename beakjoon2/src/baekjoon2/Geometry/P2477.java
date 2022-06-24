e5d5574d82825a5719098497817007f3347ba40a
kimbab2066 <qseft523125@g.yju.ac.kr>
2022-06-24 21:58:58 +0900

    2477번 참외밭

---
+++ beakjoon2/src/baekjoon2/Geometry/P2477.java
@@ -0,0 +1,17 @@
+package baekjoon2.Geometry;
+
+import java.io.*;
+import java.util.*;
+
+public class P2477 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		int num[][] = new int[6][2];
+
+		// 참외의 수
+		int K = Integer.valueOf(br.readLine());
+
+	}// end of main
+}// end of class
