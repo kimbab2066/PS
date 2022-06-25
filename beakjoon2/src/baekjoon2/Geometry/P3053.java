735ed93c71e96421269bc1f539f5e941a034dac3
kimbab2066 <qseft523125@g.yju.ac.kr>
2022-06-25 22:51:33 +0900

    3053번 택시 기하학(HERMAN)

---
+++ beakjoon2/src/baekjoon2/Geometry/P3053.java
@@ -0,0 +1,17 @@
+package baekjoon2.Geometry;
+
+import java.io.*;
+import java.util.*;
+
+public class P3053 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int R = Integer.valueOf(br.readLine());
+		double pi = Math.PI;
+		System.out.printf("%.4f", pi * R * R);
+		System.out.println();
+		System.out.println(2 * R * R);
+
+	}// end of main
+}// end of class
+//참고 : https://ahdelron.tistory.com/41
