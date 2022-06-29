9c28ceb72d161ca6a39b00c60f8c0a40c630d71c
kimbab2066 <qseft523125@g.yju.ac.kr>
2022-06-29 09:53:21 +0900

    1037번 약수

---
+++ beakjoon2/src/baekjoon2/NumberAndCombinatior/P1037.java
@@ -0,0 +1,25 @@
+package baekjoon2.NumberAndCombinatior;
+
+import java.io.*;
+import java.util.*;
+
+public class P1037 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.valueOf(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[] arr = new int[N];
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.valueOf(st.nextToken());
+		}
+		Arrays.sort(arr);
+
+		if (N >= 2) {
+			System.out.println(arr[0] * arr[N - 1]);
+		} else {
+			System.out.println(arr[0] * arr[0]);
+		}
+
+	}// end of main
+}// end of class
