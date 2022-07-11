8bab981ecd69964d3c4ecec47f2d56a94f82a740
kimbab2066 <qseft523125@g.yju.ac.kr>
2022-07-11 09:15:31 +0900

    15652번 N과 M(4)

---
+++ beakjoon2/src/baekjoon2/BackTracking/P15652.java
@@ -0,0 +1,36 @@
+package baekjoon2.BackTracking;
+
+import java.io.*;
+import java.util.*;
+
+public class P15652 {
+	static int M, N;
+	static int arr[];
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str[] = br.readLine().split(" ");
+		N = Integer.valueOf(str[0]);
+		M = Integer.valueOf(str[1]);
+		arr = new int[M];
+		DFS(1, 0);
+		System.out.println(sb);
+
+	}// end of main
+
+	static void DFS(int at, int depth) {
+		if (depth == M) {
+			for (int val : arr) {
+				sb.append(val).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+
+		for (int i = at; i <= N; i++) {
+			arr[depth] = i;
+			DFS(i, depth + 1);
+		}
+	}// end of BFS
+}// end of class
