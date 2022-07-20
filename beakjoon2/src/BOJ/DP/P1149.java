c073f783a0b6a7cddb8750284f1eadfa75355ba5
kimbab2066 <qseft523125@g.yju.ac.kr>
2022-07-20 11:20:44 +0900

    1149 RGB거리

---
+++ beakjoon2/src/BOJ/DP/P1149.java
@@ -0,0 +1,36 @@
+package BOJ.DP;
+
+import java.io.*;
+import java.util.*;
+
+public class P1149 {
+	static int N;
+	static int DP[][];
+
+	public static void main(String[] a01s) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.valueOf(br.readLine());
+		DP = new int[N][3];
+		StringTokenizer st;
+
+		// value in array
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+
+			DP[i][0] = Integer.valueOf(st.nextToken());// R
+			DP[i][1] = Integer.valueOf(st.nextToken());// G
+			DP[i][2] = Integer.valueOf(st.nextToken());// B
+		}
+
+		// MIN += 1 to N-1
+		for (int i = 1; i < N; i++) {
+			DP[i][0] += Math.min(DP[i - 1][1], DP[i - 1][2]);
+			DP[i][1] += Math.min(DP[i - 1][0], DP[i - 1][2]);
+			DP[i][2] += Math.min(DP[i - 1][0], DP[i - 1][1]);
+
+		}
+		System.out.println(Math.min(Math.min(DP[N - 1][0], DP[N - 1][1]), DP[N - 1][2]));
+	}// end of main
+
+}
+// end of class
