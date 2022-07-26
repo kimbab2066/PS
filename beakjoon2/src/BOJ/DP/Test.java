11f12924e963709dd476575f87620c46572c9f2b
kimbab2066 <qseft523125@g.yju.ac.kr>
2022-07-26 21:45:59 +0900

    동적 계획법1 테스트

---
+++ beakjoon2/src/BOJ/DP/Test.java
@@ -0,0 +1,14 @@
+package BOJ.DP;
+
+import java.io.*;
+import java.util.*;
+
+//1 OK
+public class Test {
+	static int N;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+	}// end of main
+}// end of class
