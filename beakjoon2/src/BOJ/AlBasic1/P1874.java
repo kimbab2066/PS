916d5e79e6fbba6eaa2855a42435e522953be48a
kimbab2066 <qseft523125@g.yju.ac.kr>
2022-08-01 13:01:42 +0900

    1874 스택 수열 StackClass Ver.

---
+++ beakjoon2/src/BOJ/AlBasic1/P1874.java
@@ -0,0 +1,32 @@
+package BOJ.AlBasic1;
+
+import java.io.*;
+import java.util.*;
+
+public class P1874 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.valueOf(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		Stack<Integer> stack = new Stack<>();
+		int start = 0;
+		while (N-- > 0) {
+			int val = Integer.valueOf(br.readLine());
+			if (val > start) {
+
+				for (int i = start + 1; i <= val; i++) {
+					stack.push(i);
+					sb.append("+\n");
+				}
+				start = val;
+			} else if (stack.peek() != val) {
+				System.out.println("NO");
+				return;
+			}
+			stack.pop();
+			sb.append("-\n");
+		}
+		System.out.println(sb);
+	}// end of main
+}// end of class
