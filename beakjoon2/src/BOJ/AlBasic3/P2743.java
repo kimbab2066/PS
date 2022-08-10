ae98e9f08a35071c7151348760b169c234c25fe9
kimbab2066 <qseft523125@g.yju.ac.kr>
2022-08-10 16:29:43 +0900

    2743 단어 길이 재기

---
+++ beakjoon2/src/BOJ/AlBasic3/P2743.java
@@ -0,0 +1,13 @@
+package BOJ.AlBasic3;
+
+import java.io.*;
+import java.util.*;
+
+public class P2743 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String str = br.readLine();
+		System.out.println(str.length());
+	}// end of main
+}// end of class
