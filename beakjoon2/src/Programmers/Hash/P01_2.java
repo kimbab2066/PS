069d4e099dcf8ccbe319ba40d479cb4897cbe8cc
kimbab2066 <qseft523125@g.yju.ac.kr>
2022-09-07 19:02:26 +0900

    해시 LEVEL1 완주하지 못한 선수
    배운 것 : map.getOrDefault()

---
+++ beakjoon2/src/Programmers/Hash/P01_2.java
@@ -0,0 +1,30 @@
+package Programmers.Hash;
+
+import java.io.*;
+import java.util.*;
+
+public class P01_2 {
+	public static void main(String[] args) throws IOException {
+		String participant[] = { "mislav", "stanko", "mislav", "ana" };
+		String completion[] = { "stanko", "ana", "mislav" };
+
+		String answer = "";
+		HashMap<String, Integer> map = new HashMap<>();
+		for (String player : participant) {
+			map.put(player, map.getOrDefault(player, 0) + 1);
+		}
+		System.out.println(map);
+		for (String player : completion) {
+			map.put(player, map.get(player) - 1);
+		}
+		System.out.println(map);
+
+		for (String key : map.keySet()) {
+			if (map.get(key) != 0) {
+				answer = key;
+			}
+		}
+		System.out.println(answer);
+	}// end of main
+}
+// end of class
