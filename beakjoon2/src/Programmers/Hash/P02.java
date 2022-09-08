773de12a7eb391e1e4501a08e8e365ab99a714c4
kimbab2066 <qseft523125@g.yju.ac.kr>
2022-09-08 11:25:02 +0900

    프로그래머스 피보나치 수

---
+++ beakjoon2/src/Programmers/Hash/P02.java
@@ -0,0 +1,17 @@
+package Programmers.Hash;
+
+import java.util.HashMap;
+import java.util.Iterator;
+
+public class P02 {
+	public static void main(String[] args) {
+		int nums[] = { 3, 1, 2, 3 };
+		int answer = 0;
+		HashMap<Integer, Integer> map = new HashMap<>();
+		for (int pocket : nums) {
+			map.put(pocket, 1);
+		}
+		answer = map.size() > nums.length / 2 ? nums.length / 2 : map.size();
+		System.out.println(answer);
+	}// end of main
+}// end of class
