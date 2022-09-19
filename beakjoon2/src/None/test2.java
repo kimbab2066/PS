856810347ab27ffb47228ea2c46825059b0d7436
kimbab2066 <qseft523125@g.yju.ac.kr>
2022-09-19 14:13:19 +0900

    알고리즘 스터디 피보나치 수

--- beakjoon2/src/None/test2.java
+++ beakjoon2/src/None/test2.java
@@ -4,28 +4,23 @@ import java.io.*;
 import java.util.*;
 
 public class test2 {
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.valueOf(br.readLine());
 		int arr[] = new int[N + 1];
-		arr[1] = arr[2] = 1;
 		int cnt = 0;
-		int cnt2 = 0;
+		arr[1] = arr[2] = 1;
+
 		for (int i = 3; i <= N; i++) {
 			arr[i] = arr[i - 1] + arr[i - 2];
-			cnt2++;
+			cnt++;
 		}
-		cnt = fib(N);
-		System.out.println(arr[N] + " " + cnt2);
+		System.out.println(fib(N) + " " + cnt);
 	}// end of main
 
 	static int fib(int n) {
-		int cnt = 0;
-		if (n == 1 || n == 2) {
-			return 1;
-		} else {
-			cnt++;
-			return (fib(n - 1) + fib(n - 2));
-		}
+		if (n == 1 || n == 2) return 1;
+		return (fib(n - 1) + fib(n - 2));
 	}
 }// end of class
