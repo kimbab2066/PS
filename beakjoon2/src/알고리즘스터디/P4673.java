7f63857992fbb7ed0fcd22784461b0d3802d4281
kimbab2066 <qseft523125@g.yju.ac.kr>
2022-09-19 13:30:08 +0900

    알고리즘 스터디 4673 셀프넘버

---
+++ beakjoon2/src/알고리즘스터디/P4673.java
@@ -0,0 +1,31 @@
+package 알고리즘스터디;
+
+import java.io.*;
+import java.util.*;
+
+public class P4673 {
+	public static void main(String[] args) throws IOException {
+		boolean check[] = new boolean[10001];
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 1; i <= 10000; i++) {
+			int d = self_number(i);
+			if (d < 10001) {
+				check[d] = true;
+			}
+			if (!check[i])
+				sb.append(i).append("\n");
+		}
+		System.out.println(sb);
+
+	}// end of main
+
+	static int self_number(int i) {
+		int result = i;
+		while (i > 0) {
+			result += i % 10;
+			i /= 10;
+		}
+		return result;
+	}// end of self_number
+}// end of class
