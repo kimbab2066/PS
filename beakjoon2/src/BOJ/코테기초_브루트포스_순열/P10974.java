dcd6f32e29a6f318633ffc88e116391bc5ef8ff7
kimbab2066 <qseft523125@g.yju.ac.kr>
2022-09-27 18:44:06 +0900

    10974 모든 순열

---
+++ beakjoon2/src/BOJ/코테기초_브루트포스_순열/P10974.java
@@ -0,0 +1,37 @@
+package BOJ.코테기초_브루트포스_순열;
+
+import java.io.*;
+
+public class P10974 {
+	static int N;
+	static int[] arr;
+	static boolean[] visited;
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.valueOf(br.readLine());
+		arr = new int[N];
+		visited = new boolean[N];
+		permutation(0);
+		System.out.println(sb);
+	}// end of main
+
+	static void permutation(int depth) {
+		if (depth == N) {
+			for (int val : arr) {
+				sb.append(val + " ");
+			}
+			sb.append("\n");
+			return;
+		}
+		for (int i = 0; i < N; i++) {
+			if (!visited[i]) {
+				arr[depth] = i + 1;
+				visited[i] = true;
+				permutation(depth + 1);
+				visited[i] = false;
+			}
+		}
+	}// end of permutation
+}// end of class
