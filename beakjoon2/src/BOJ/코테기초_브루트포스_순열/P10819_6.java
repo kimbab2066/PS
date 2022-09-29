04565fd3bdee33f10bfe8b68069ac474a810f388
kimbab2066 <qseft523125@g.yju.ac.kr>
2022-09-29 10:39:42 +0900

    10819 차이를 최대로(re) DFS+완전탐색

---
+++ beakjoon2/src/BOJ/코테기초_브루트포스_순열/P10819_6.java
@@ -0,0 +1,34 @@
+package BOJ.코테기초_브루트포스_순열;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class P10819_6 {
+	static int N, answer;
+	static int[] arr;
+	static boolean[] visited;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.valueOf(br.readLine());
+		arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::valueOf).toArray();
+		visited = new boolean[N];
+
+		dfs(0, 0, 0);
+		System.out.println(answer);
+	}// end of main
+
+	static void dfs(int cnt, int bf, int sum) {
+		if (cnt == N) {
+			answer = Math.max(sum, answer);
+			return;
+		}
+		for (int i = 0; i < N; i++) {
+			if (!visited[i]) {
+				visited[i] = true;
+				bfs(cnt + 1, arr[i], cnt == 0 ? 0 : sum + Math.abs(bf - arr[i]));
+				visited[i] = false;
+			}
+		}
+	}// end of bfs
+}// end of class
