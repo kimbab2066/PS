e3101fbf7e136a02cbfa1d0f7b40b84b0dec5ab8
kimbab2066 <qseft523125@g.yju.ac.kr>
2022-10-28 20:22:08 +0900

    카드 뒤집기 게임 (구현)

--- beakjoon2/src/스터디/Test1.java
+++ beakjoon2/src/스터디/Test1.java
@@ -1,43 +1,50 @@
 package 스터디;
 
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+//구현 핵심은 비트마스킹연산임
 public class Test1 {
-	static int N, A, B;
+	static int N, A, B, cnt;
 	static int[] arr, scores;
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		input();
 		logic();
-		output();
+//		output();
 	} // end of main
 
 	static void logic() {
 		// 모든 경우의 수 = 2의 N제곱
-		int cases = (int) Math.pow(2, N);
-		scores = new int[cases];
-		boolean bF = true;
-		int cnt = N;// plus의 개수
-		for (int i = 0; i < cases; i++) {
-			for (int j = 0; j < cnt; j++) {
-				scores[i] += arr[j];
+		for (int i = 0; i < (1 << arr.length); i++) {
+			scores = arr.clone();
+			int sum = 0;
+			for (int j = 0; j < arr.length; j++) {
+				if (((1 << j) & i) != 0) {
+					scores[j] = -scores[j];
+				}
+				sum += scores[j];
 			}
-			cnt--;
+			if (A <= sum && sum <= B) cnt++;
 		}
-		System.out.println(Arrays.toString(scores));
+
+		System.out.println(cnt);
 	}// end of logic
 
-	static void input() {
-		Scanner in = new Scanner(System.in);
-		N = in.nextInt();
+	static void input() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.valueOf(br.readLine());
 		StringBuilder sb = new StringBuilder();
 		arr = new int[N];
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; i++) {
-			arr[i] = in.nextInt();
+			arr[i] = Integer.valueOf(st.nextToken());
 		}
-		A = in.nextInt();
-		B = in.nextInt();
+		st = new StringTokenizer(br.readLine());
+		A = Integer.valueOf(st.nextToken());
+		B = Integer.valueOf(st.nextToken());
 	}// end of input
 
 	static void output() {
@@ -47,7 +54,7 @@ public class Test1 {
 				cnt++;
 			}
 		}
-		System.out.println(cnt);
+//		System.out.println(cnt);
 	}// end of output
 }// end of class
 // 3장이 있을 때 8개의 경우의 수가 나옴
