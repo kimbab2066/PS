496a61fc6133a9f91242353ceb8647532f0cd3b3
kimbab2066 <qseft523125@g.yju.ac.kr>
2022-11-17 16:51:25 +0900

    1021 유기농 배추

--- beakjoon2/src/BOJ/자료구조/P1021.java
+++ beakjoon2/src/BOJ/자료구조/P1021.java
@@ -1,7 +1,15 @@
 package BOJ.자료구조;
 
-public class P1021 {
-	public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-	}
-}
+public class P1021 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		// N은 1~N을 의미함, 뽑으려는 수의 개수 M
+		// 2,3번 연산의 최솟값을 출력해라
+		
+		
+	}// end of main
+}// end of class
