13f3a2a84f63c69df08043f34ff85af222570f68
kimbab2066 <qseft523125@g.yju.ac.kr>
2022-11-21 20:48:50 +0900

    10026 적록색약

--- beakjoon2/src/BOJ/BFS/P10026.java
+++ beakjoon2/src/BOJ/BFS/P10026.java
@@ -7,11 +7,11 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class P10026 {
-	static char[][] color = new char[100][100];
-	static boolean[][] vis = new boolean[100][100];
+	static char[][] color = new char[101][101];
+	static boolean[][] vis = new boolean[101][101];
 	static int[] dx = { 1, 0, -1, 0 };
 	static int[] dy = { 0, 1, 0, -1 };
-	static int N, cnt;
+	static int N;
 	static Queue<int[]> q = new LinkedList<>();
 
 	public static void main(String[] args) throws IOException {
@@ -21,17 +21,22 @@ public class P10026 {
 		// value in array
 		for (int i = 0; i < N; i++) {
 			color[i] = br.readLine().toCharArray();
+		}
+		// 색약 아닌 사람
+		int not_color = color();
+		// 방문 배열 초기화
+		vis = new boolean[101][101];
+		//
+		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
-				if (color[i][j] == 'R') {
-					bfs(i, j);
-				} else if (color[i][j] == 'G') {
-					bfs(i, j);
-				} else if (color[i][j] == 'B') {
-					bfs(i, j);
+				if (color[i][j] == 'G') {
+					color[i][j] = 'R';
 				}
 			}
 		} //
-		System.out.println(cnt);
+		int color = color();
+
+		System.out.println(not_color + " " + color);
 	}// main
 
 	static void bfs(int x, int y) {
@@ -43,11 +48,26 @@ public class P10026 {
 				int nx = node[0] + dx[d];
 				int ny = node[1] + dy[d];
 				// 범위를 벗어나는 경우
-				if (nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
-				if (vis[nx][ny] || color[x][y] != color[nx][ny]) continue;
+				if (nx < 0 || ny < 0 || nx >= N || ny >= N)
+					continue;
+				if (vis[nx][ny] || color[x][y] != color[nx][ny])
+					continue;
 				vis[nx][ny] = true;
 				q.add(new int[] { nx, ny });
 			}
-		}// while
+		} // while
 	}// bfs
+
+	static int color() {
+		int cnt = 0;
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if (!vis[i][j]) {
+					cnt++;
+					bfs(i, j);
+				}
+			}
+		}
+		return cnt;
+	}// not_color
 }// class
